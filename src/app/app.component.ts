fa7daf604101e1398c616da8bec546e2c12c451e
Jorge Vilca <jlvilcav@gmail.com>
2022-07-28 19:56:24 -0500

    Event que tambien se pueden escuchar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,4 +28,15 @@ export class AppComponent {
   decreaseAge() {
     this.person.age--;
   }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+
+  }
 }
